9f37e3a89f70503855c02c723008bb93a66603bf
Sleepyy Stark <96537491+TeraByteX1@users.noreply.github.com>
2023-01-11 15:59:21 +0100

    Add files via upload

---
+++ Quiz-Application-main/js/questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Wie publiceert Call Of Duty?",
+    answer: "Activision",
+    options: [
+      "Electronic Arts",
+      "Valve",
+      "Activision",
+      "Microsoft"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Wanneer werd de eerste Call of Duty-titel officieel uitgebracht?",
+    answer: "2003",
+    options: [
+      "2003",
+      "2007",
+      "2010",
+      "2001"
+    ]
+  },
+    {
+    numb: 3,
+    question: "In welke CoD-serie zijn exosuits geïntroduceerd?",
+    answer: "Advanced Warfare",
+    options: [
+      "World At War",
+      "Vanguard",
+      "Black Ops",
+      "Advanced Warfare"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Waar vindt de openingsscène in Modern Warfare 3 plaats?",
+    answer: "Financial District NY",
+    options: [
+      "Financial District Hong Kong",
+      "Financial District NY",
+      "Washington Monument, Washington DC",
+      "Bay of Pigs Cuba"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Wat is het doel in de gameplay-modus Domination?",
+    answer: "Capture the Flag",
+    options: [
+      "Kill all other players",
+      "Capture the Flag",
+      "Every Player for Himself",
+      "Detonate certain bomb areas"
+    ]
+  },
+];
