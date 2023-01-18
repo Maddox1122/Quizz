79804ac500ea9dcd6a48784b531f3e1389b3bc70
Maddox de Rooij <89667@glr.nl>
2023-01-18 19:52:09 +0100

    Script voor quiz verander

--- Maddox quiz/quizval.js
+++ Maddox quiz/quizval.js
@@ -1,3 +1,224 @@
 let score = 0
 
-function check(antwoord, vraag)
+function check(antwoord, vraag) {
+    if (vraag == 1) {
+        if (antwoord == 'goed') {
+            score++
+            document.getElementById('button1g').style.background = '#13df50'
+            document.getElementById('button1.1').style.background = '#af230a'
+            document.getElementById('button1.2').style.background = '#af230a'
+            document.getElementById('button1.3').style.background = '#af230a'
+            document.getElementById('button1g').disabled = true
+            document.getElementById('button1.1').disabled = true
+            document.getElementById('button1.2').disabled = true
+            document.getElementById('button1.3').disabled = true
+        } else if (antwoord == 'fout') {
+            document.getElementById('button1g').style.background = '#13df50'
+            document.getElementById('button1.1').style.background = '#af230a'
+            document.getElementById('button1.2').style.background = '#af230a'
+            document.getElementById('button1.3').style.background = '#af230a'
+            document.getElementById('button1g').disabled = true
+            document.getElementById('button1.1').disabled = true
+            document.getElementById('button1.2').disabled = true
+            document.getElementById('button1.3').disabled = true
+        }
+    }
+    if (vraag == 2) {
+        if (antwoord == 'goed') {
+            score++
+            document.getElementById('button2g').style.background = '#13df50'
+            document.getElementById('button2.1').style.background = '#af230a'
+            document.getElementById('button2.2').style.background = '#af230a'
+            document.getElementById('button2.3').style.background = '#af230a'
+            document.getElementById('button2g').disabled = true
+            document.getElementById('button2.1').disabled = true
+            document.getElementById('button2.2').disabled = true
+            document.getElementById('button2.3').disabled = true
+        } else if (antwoord == 'fout') {
+            document.getElementById('button2g').style.background = '#13df50'
+            document.getElementById('button2.1').style.background = '#af230a'
+            document.getElementById('button2.2').style.background = '#af230a'
+            document.getElementById('button2.3').style.background = '#af230a'
+            document.getElementById('button2g').disabled = true
+            document.getElementById('button2.1').disabled = true
+            document.getElementById('button2.2').disabled = true
+            document.getElementById('button2.3').disabled = true
+        }
+    }
+    if (vraag == 3) {
+        if (antwoord == 'goed') {
+            score++
+            document.getElementById('button3g').style.background = '#13df50'
+            document.getElementById('button3.1').style.background = '#af230a'
+            document.getElementById('button3.2').style.background = '#af230a'
+            document.getElementById('button3.3').style.background = '#af230a'
+            document.getElementById('button3g').disabled = true
+            document.getElementById('button3.1').disabled = true
+            document.getElementById('button3.2').disabled = true
+            document.getElementById('button3.3').disabled = true
+        } else if (antwoord == 'fout') {
+            document.getElementById('button3g').style.background = '#13df50'
+            document.getElementById('button3.1').style.background = '#af230a'
+            document.getElementById('button3.2').style.background = '#af230a'
+            document.getElementById('button3.3').style.background = '#af230a'
+            document.getElementById('button3g').disabled = true
+            document.getElementById('button3.1').disabled = true
+            document.getElementById('button3.2').disabled = true
+            document.getElementById('button3.3').disabled = true
+        }
+    }
+    if (vraag == 4) {
+        if (antwoord == 'goed') {
+            score++
+            document.getElementById('button4g').style.background = '#13df50'
+            document.getElementById('button4.1').style.background = '#af230a'
+            document.getElementById('button4.2').style.background = '#af230a'
+            document.getElementById('button4.3').style.background = '#af230a'
+            document.getElementById('button4g').disabled = true
+            document.getElementById('button4.1').disabled = true
+            document.getElementById('button4.2').disabled = true
+            document.getElementById('button4.3').disabled = true
+        } else if (antwoord == 'fout') {
+            document.getElementById('button4g').style.background = '#13df50'
+            document.getElementById('button4.1').style.background = '#af230a'
+            document.getElementById('button4.2').style.background = '#af230a'
+            document.getElementById('button4.3').style.background = '#af230a'
+            document.getElementById('button4g').disabled = true
+            document.getElementById('button4.1').disabled = true
+            document.getElementById('button4.2').disabled = true
+            document.getElementById('button4.3').disabled = true
+        }
+    }
+    if (vraag == 5) {
+        if (antwoord == 'goed') {
+            score++
+            document.getElementById('button5g').style.background = '#13df50'
+            document.getElementById('button5.1').style.background = '#af230a'
+            document.getElementById('button5.2').style.background = '#af230a'
+            document.getElementById('button5.3').style.background = '#af230a'
+            document.getElementById('button5g').disabled = true
+            document.getElementById('button5.1').disabled = true
+            document.getElementById('button5.2').disabled = true
+            document.getElementById('button5.3').disabled = true
+        } else if (antwoord == 'fout') {
+            document.getElementById('button5g').style.background = '#13df50'
+            document.getElementById('button5.1').style.background = '#af230a'
+            document.getElementById('button5.2').style.background = '#af230a'
+            document.getElementById('button5.3').style.background = '#af230a'
+            document.getElementById('button5g').disabled = true
+            document.getElementById('button5.1').disabled = true
+            document.getElementById('button5.2').disabled = true
+            document.getElementById('button5.3').disabled = true
+        }
+    }
+    if (vraag == 6) {
+        if (antwoord == 'goed') {
+            score++
+            document.getElementById('button6g').style.background = '#13df50'
+            document.getElementById('button6.1').style.background = '#af230a'
+            document.getElementById('button6.2').style.background = '#af230a'
+            document.getElementById('button6.3').style.background = '#af230a'
+            document.getElementById('button6g').disabled = true
+            document.getElementById('button6.1').disabled = true
+            document.getElementById('button6.2').disabled = true
+            document.getElementById('button6.3').disabled = true
+        } else if (antwoord == 'fout') {
+            document.getElementById('button6g').style.background = '#13df50'
+            document.getElementById('button6.1').style.background = '#af230a'
+            document.getElementById('button6.2').style.background = '#af230a'
+            document.getElementById('button6.3').style.background = '#af230a'
+            document.getElementById('button6g').disabled = true
+            document.getElementById('button6.1').disabled = true
+            document.getElementById('button6.2').disabled = true
+            document.getElementById('button6.3').disabled = true
+        }
+    }
+    if (vraag == 7) {
+        if (antwoord == 'goed') {
+            score++
+            document.getElementById('button7g').style.background = '#13df50'
+            document.getElementById('button7.1').style.background = '#af230a'
+            document.getElementById('button7.2').style.background = '#af230a'
+            document.getElementById('button7.3').style.background = '#af230a'
+            document.getElementById('button7g').disabled = true
+            document.getElementById('button7.1').disabled = true
+            document.getElementById('button7.2').disabled = true
+            document.getElementById('button7.3').disabled = true
+        } else if (antwoord == 'fout') {
+            document.getElementById('button7g').style.background = '#13df50'
+            document.getElementById('button7.1').style.background = '#af230a'
+            document.getElementById('button7.2').style.background = '#af230a'
+            document.getElementById('button7.3').style.background = '#af230a'
+            document.getElementById('button7g').disabled = true
+            document.getElementById('button7.1').disabled = true
+            document.getElementById('button7.2').disabled = true
+            document.getElementById('button7.3').disabled = true
+        }
+    }
+    if (vraag == 8) {
+        if (antwoord == 'goed') {
+            score++
+            document.getElementById('button8g').style.background = '#13df50'
+            document.getElementById('button8.1').style.background = '#af230a'
+            document.getElementById('button8.2').style.background = '#af230a'
+            document.getElementById('button8.3').style.background = '#af230a'
+            document.getElementById('button8g').disabled = true
+            document.getElementById('button8.1').disabled = true
+            document.getElementById('button8.2').disabled = true
+            document.getElementById('button8.3').disabled = true
+        } else if (antwoord == 'fout') {
+            document.getElementById('button8g').style.background = '#13df50'
+            document.getElementById('button8.1').style.background = '#af230a'
+            document.getElementById('button8.2').style.background = '#af230a'
+            document.getElementById('button8.3').style.background = '#af230a'
+            document.getElementById('button8g').disabled = true
+            document.getElementById('button8.1').disabled = true
+            document.getElementById('button8.2').disabled = true
+            document.getElementById('button8.3').disabled = true
+        }
+    }
+    if (vraag == 9) {
+        if (antwoord == 'goed') {
+            score++
+            document.getElementById('button9g').style.background = '#13df50'
+            document.getElementById('button9.1').style.background = '#af230a'
+            document.getElementById('button9.2').style.background = '#af230a'
+            document.getElementById('button9.3').style.background = '#af230a'
+            document.getElementById('button9g').disabled = true
+            document.getElementById('button9.1').disabled = true
+            document.getElementById('button9.2').disabled = true
+            document.getElementById('button9.3').disabled = true
+        } else if (antwoord == 'fout') {
+            document.getElementById('button9g').style.background = '#13df50'
+            document.getElementById('button9.1').style.background = '#af230a'
+            document.getElementById('button9.2').style.background = '#af230a'
+            document.getElementById('button9.3').style.background = '#af230a'
+            document.getElementById('button9g').disabled = true
+            document.getElementById('button9.1').disabled = true
+            document.getElementById('button9.2').disabled = true
+            document.getElementById('button9.3').disabled = true
+        }
+    }
+    if (vraag == 10) {
+        if (antwoord == 'goed') {
+            score++
+            document.getElementById('button10g').style.background = '#13df50'
+            document.getElementById('button10.1').style.background = '#af230a'
+            document.getElementById('button10.2').style.background = '#af230a'
+            document.getElementById('button10.3').style.background = '#af230a'
+            document.getElementById('button10g').disabled = true
+            document.getElementById('button10.1').disabled = true
+            document.getElementById('button10.2').disabled = true
+            document.getElementById('button10.3').disabled = true
+        } else if (antwoord == 'fout') {
+            document.getElementById('button10g').style.background = '#13df50'
+            document.getElementById('button10.1').style.background = '#af230a'
+            document.getElementById('butto10.2').style.background = '#af230a'
+            document.getElementById('button10.3').style.background = '#af230a'
+            document.getElementById('button10g').disabled = true
+            document.getElementById('button10.1').disabled = true
+            document.getElementById('button10.2').disabled = true
+            document.getElementById('button10.3').disabled = true
+        }
+    }
+}
